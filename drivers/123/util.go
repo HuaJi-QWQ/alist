189353f38fce03a2f25f1946ae3cd7d3fc1cf21a
LazyBones <141557712+HuaJi-QWQ@users.noreply.github.com>
2024-10-01 11:42:43 +0800

    Update util.go

--- drivers/123/util.go
+++ drivers/123/util.go
@@ -162,9 +162,10 @@ func (d *Pan123) login() error {
 		SetHeaders(map[string]string{
 			"origin":      "https://www.123pan.com",
 			"referer":     "https://www.123pan.com/",
-			"user-agent":  "Dart/2.19(dart:io)-alist",
-			"platform":    "web",
-			"app-version": "3",
+			"user-agent":  "123pan/v2.4.0(Android_7.1.2;Xiaomi)",
+			"platform":    "android",
+			"x-app-version": "2.4.0"
+			"app-version": "61",
 			//"user-agent":  base.UserAgent,
 		}).
 		SetBody(body).Post(SignIn)
@@ -199,9 +200,10 @@ func (d *Pan123) request(url string, method string, callback base.ReqCallback, r
 		"origin":        "https://www.123pan.com",
 		"referer":       "https://www.123pan.com/",
 		"authorization": "Bearer " + d.AccessToken,
-		"user-agent":    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) alist-client",
-		"platform":      "web",
-		"app-version":   "3",
+		"user-agent":    "123pan/v2.4.0(Android_7.1.2;Xiaomi)",
+		"platform":      "android",
+                "x-app-version": "2.4.0",
+		"app-version":   "61",
 		//"user-agent":    base.UserAgent,
 	})
 	if callback != nil {
